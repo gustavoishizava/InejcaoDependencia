df8f8813629aba3170936e8ecadc6f9b59f4043b
Gustavo C. Ishizava <gustavoishizava@hotmail.com>
2022-02-08 21:28:38 -0300

    remove unecessary usings

--- InjecaoDependencia/Workers/Worker.cs
+++ InjecaoDependencia/Workers/Worker.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using InjecaoDependencia.Application;
-using InjecaoDependencia.Service;
 
 namespace InjecaoDependencia.Workers
 {
